33f3e7bc8db4cb20616b78b74312859075dd2bdc
fawltea <jake.stocker@protonmail.com>
2024-03-08 19:12:36 +0000

    init commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const axios = require('axios');
+
+const apiKey = '';
+const apiUrl = 'https://api.stormglass.io/v2/weather/point';
+
+const getSurfForecast = async (latitude, longitude) => {
+  try {
+    const response = await axios.get(apiUrl, {
+      params: {
+        lat: latitude,
+        lng: longitude,
+        params: 'swellHeight,swellPeriod,swellDirection',
+      },
+      headers: {
+        Authorization: apiKey,
+      },
+    });
+
+    const forecastData = response.data;
+    console.log(forecastData);
+  } catch (error) {
+    console.error('Error fetching surf forecast:', error.message);
+  }
+};
+
+const beachLatitude = 51.4877;
+const beachLongitude = 3.7282;
+
+getSurfForecast(beachLatitude, beachLongitude);
